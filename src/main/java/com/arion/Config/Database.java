e7ea9c0297f157c48ad7b731d162727727971e0b
Erick <ericklararamirez9@gmail.com>
2025-09-01 19:12:09 -0600

    Conexion a la base de datos

--- src/main/java/com/arion/Config/Database.java
+++ src/main/java/com/arion/Config/Database.java
@@ -1,5 +1,35 @@
 package com.arion.Config;
 
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class Database {
 
+    private static Connection connection;
+
+    // Método para obtener la conexión
+    public static Connection getConnection() throws Exception {
+        if (connection == null || connection.isClosed()) {
+            // Leer archivo config.properties
+            Properties props = new Properties();
+            try (InputStream input = Database.class.getResourceAsStream("/config.properties")) {
+                if (input == null) {
+                    throw new Exception("No se encontró el archivo config.properties");
+                }
+                props.load(input);
+            }
+
+            String url = props.getProperty("db.url");
+            String user = props.getProperty("db.user");
+            String password = props.getProperty("db.password");
+
+            // Conectar a la base de datos
+            connection = DriverManager.getConnection(url, user, password);
+            System.out.println("Conexión a la base de datos establecida.");
+        }
+        return connection;
+    }
 }
